fee5de3b802e1b5d16bf06f4fc353ed9dac15394
Nick Faulkner <nickbfaulkner@gmail.com>
2017-04-18 17:28:18 +0100

    initial version

---
+++ files/install-msssql-tools.sh
@@ -0,0 +1,6 @@
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/msprod.list
+apt-get update
+apt-get install mssql-tools
+echo =======================================
+echo /opt/mssql-tools/bin/sqlcmd
